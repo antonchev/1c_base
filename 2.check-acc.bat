fa24653405270bc2381c1f9dff94e79359956baf
aantonchev@ivi.ru <aantonchev@ivi.ru>
2021-12-23 15:27:58 +0300

    инициализация

---
+++ 2.check-acc.bat
@@ -0,0 +1,4 @@
+chcp 65001
+vrunner run --command "acc.propertiesPaths=tools\acc-export\acc.properties;" --execute "tools\acc-export\acc-export.epf" --ordinaryapp 1 --ibconnection "/Sdev-1s-srv\acc" --db-user "Администратор" 
+
+pause
